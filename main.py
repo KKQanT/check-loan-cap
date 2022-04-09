9119381e30d8a441a4fd91d5f30571a20dafb481
peerakarn <peerakarn.jit@gmail.com>
2022-04-09 11:29:35 +0700

    add cors

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import mysql.connector
 import pandas as pd
 import numpy as np
 import os
 from dotenv import load_dotenv
 
+
 app = FastAPI()
+
+origins = [
+  "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_dotenv()
 
