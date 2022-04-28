9a34dd22179511fa45127b65f98ee1a8019cfd96
peerakarn <peerakarn.jit@gmail.com>
2022-04-28 12:42:36 +0700

    add check sol and new pacjkage

--- main.py
+++ main.py
@@ -63,7 +63,11 @@ def get_total_optin():
     2:1,
     3:2,
     4:3,
-    5:4
+    5:4,
+    6:1,
+    7:2,
+    8:3,
+    9:4
   }
 
   df['solLend'] = df['loanPackage'].map(sol_to_send)
@@ -84,3 +88,56 @@ def get_hashlist(daos_name:str):
   with open(f'tokens-hashlist/{daos_name}.json', 'r') as json_file:
     tokens_hashlist = json.load(json_file)
   return tokens_hashlist
+
+@app.get('/get-sol-cap/')
+
+def get_sol_cap():
+  MIN_ID = os.getenv('MIN_ID_TO_CAL_CAP')
+  MIN_ID = int(MIN_ID)
+
+  DB_HOST = os.getenv('DB_HOST')
+  DB_USER = os.getenv('DB_USER')
+  DB_PASSWORD = os.getenv('DB_PASSWORD')
+  
+  MAX_SOL = os.getenv('MAX_SOL')
+  MAX_SOL = int(MAX_SOL)
+
+  is_exceed = False
+
+  connection = mysql.connector.connect(
+  host=DB_HOST,
+  user=DB_USER,
+  password=DB_PASSWORD
+  )
+
+  cursor = connection.cursor()
+  cursor.execute(f"SELECT * FROM lendSubmitV1 WHERE id >= {MIN_ID}")
+  records = cursor.fetchall()
+  df = pd.DataFrame(data=records, columns=[c[0] for c in cursor.description])
+
+  df = df.drop_duplicates(subset=['discordId'], keep='last')
+  df = df.drop_duplicates(subset=['walletAddress'], keep='last').reset_index(drop=True)
+
+  sol_to_send = {
+    1:np.nan,
+    2:1,
+    3:2,
+    4:3,
+    5:4,
+    6:1,
+    7:2,
+    8:3,
+    9:4
+  }
+
+  df['solLend'] = df['loanPackage'].map(sol_to_send)
+  df = pd.DataFrame(df[df['solLend'].isna() == False]).reset_index(drop=True)
+
+  total_sol = df['solLend'].sum()
+
+  if total_sol >= MAX_SOL:
+    is_exceed = True
+  else:
+    is_exceed = False
+
+  return {'totalSol':total_sol, "isExceed":is_exceed}
